c04a752349992b2b2c3864d55b42c44c887748dc
laura villa <99909063+lau0614@users.noreply.github.com>
2022-05-12 08:44:50 -0500

    cambios 12 mayo

--- practica.js
+++ practica.js
@@ -1,1 +1,17 @@
-
+let num1= parseInt (prompt ("escribe un numero: "));
+let num2= parseInt (prompt ("escribe otro numero: "));
+let operacion = prompt("escribe cual operacion deseas realizar entre los dos numeros escritos antes: ");
+let resultado = 0;
+if (operacion == "sumar" ) {
+    resultado = num1 + num2;
+}
+if (operacion == "restar" ) {
+    resultado = num1 - num2;
+}
+if (operacion == "multiplicar" ) {
+    resultado = num1 * num2;
+}
+if (operacion == "dividir" ) {
+    resultado = num1 / num2;
+}
+alert ("el resultado de tu operacion es: " + resultado);
