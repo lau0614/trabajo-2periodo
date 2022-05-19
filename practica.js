3152439f2b15d73dd1126de6bb31d14d230268cc
laura villa <99909063+lau0614@users.noreply.github.com>
2022-05-19 08:51:03 -0500

    trabajo 2 clase

--- practica.js
+++ practica.js
@@ -15,3 +15,30 @@ if (operacion == "dividir" ) {
     resultado = num1 / num2;
 }
 alert ("el resultado de tu operacion es: " + resultado);
+
+
+
+
+let num1= parseInt(prompt("Escribe un numero: "));
+let num2= parseInt(prompt("Porfavor escribe otro numero: "));
+let operaciones = prompt("Escribe cual operacion deseas realizar entre los dos numero que acabaste de escribir: ");
+/* let resultado = 0;
+if(operaciones == "suma" || operaciones == "+"){
+     resultado = num1 + num2;
+}
+else
+if(operaciones == "restar" || operaciones == "-"){
+    resultado = num1 - num2;
+}
+else
+    if(operaciones == "multiplicar" || operaciones == "*"){
+        resultado = num1 * num2;
+    }
+    else
+        if(operaciones == "dividir" || operaciones == "/"){
+            resultado = num1 / num2;
+        } */
+       
+
+        resultado= operaciones == "suma" || operaciones == "+" ? num1 + num2:   operaciones == "restar" || operaciones == "-" ? num1 - num2:   operaciones == "multiplicar" || operaciones == "*" ? num1 - num2: num1 - num2 ;
+        alert("El resultado de tu operacion es: " + resultado);
